2f995e0890eaa8ad2b0ca80431dc849da64a5c09
github username <ingrey1@gmail.com>
2020-01-03 15:46:04 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,14 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    value1 = Kernel.rand(0, 2)
+    value2 = Kernel.rand(0, 2)
     resp.write "Hello, World"
+    if value1 == value2
+       resp.write "You Win"
+    else 
+      resp.write "You Lose"
+    end 
     resp.finish
   end
 
